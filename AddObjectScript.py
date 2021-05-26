5e7a7a2dd9755c1e7caf0ef144d09e332a39e8e5
Kryštof <48121710+krystofex@users.noreply.github.com>
2021-05-26 19:48:47 +0200

    prototype

--- AddObjectScript.py
+++ AddObjectScript.py
@@ -1,28 +1,22 @@
-import bpy
 
-class TestPanel(bpy.types.Panel):
-    bl_label = "Medal generator"
-    bl_idname = "PT_TestPanel"
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
-    bl_category = 'Medal'
-    
-    def draw(self, context):
-        layout = self.layout
-                
-            
-        row = layout.row()
-        row.operator("mesh.primitive_cylinder_add",icon='NODE_MATERIAL', text="create medal")
-             
+medalHeight = 0.1
+medalRadius = 1
+yourText = "congratulations"
 
-def register():
-    bpy.utils.register_class(TestPanel)
-    
-    
-def unregister():
-    bpy.utils.unregister_class(TestPanel)
-    
-    
-if __name__ == "__main__":
-    register()
-    
+bpy.ops.mesh.primitive_cylinder_add(
+    vertices=256, radius=medalRadius, depth=medalHeight, enter_editmode=False, align='WORLD', location=(0, 0, 0))
+
+bpy.ops.curve.primitive_bezier_circle_add(
+    radius=1, enter_editmode=False, align='WORLD', location=(0, 0, 0))
+
+bpy.ops.object.text_add()
+ob = bpy.context.object
+ob.data.body = yourText
+textSize = medalRadius * 2 - 0.1
+bpy.context.object.dimensions[0] = textSize
+bpy.context.object.dimensions[1] = bpy.context.object.scale[0]
+bpy.context.object.dimensions[2] = bpy.context.object.scale[0]
+
+bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY', center='BOUNDS')
+bpy.context.object.location[0] = 0
+bpy.context.object.location[2] = medalHeight/2
